ed3423e4d297c4a07d0db681d7d45ef219c3944b
LE Van Tuan <levantuan2312@gmail.com>
2020-09-18 09:05:36 +0200

    [setup] 0.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def local_file(file):
 
 setuptools.setup(
     name="people_also_ask",
-    version="0.0.3",
+    version="0.0.4",
     author="LE Van Tuan",
     author_email="leavantuan2312@gmail.com",
     packages=setuptools.find_packages(),
